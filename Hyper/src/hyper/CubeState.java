aed06d116e86a0a6aa1a03aace127532e4214322
deblau <flickboy@hotmail.com>
2016-01-23 11:48:14 -0500

    Added vacancy and neighbor adding helper functions

--- Hyper/src/hyper/CubeState.java
+++ Hyper/src/hyper/CubeState.java
@@ -1,5 +1,6 @@
 package hyper;
 
+import java.math.BigInteger;
 import java.util.Vector;
 
 /**
@@ -26,4 +27,16 @@ class CubeState
 		return "Dim: " + dim + "\nMy address: " + addr + "\nLinks/Neighbors: " + links + ", " + neighbors.toString();
 	}
 
+	// Do I have any vacant links?
+	boolean vacancy()
+	{
+		return !BigInteger.ZERO.setBit(dim).subtract(BigInteger.ONE).equals((BigInteger) links);
+	}
+
+	// Add a neighbor
+	void addNeighbor(int link, Neighbor n)
+	{
+		neighbors.add(link, n);
+		links = new CubeAddress(links.setBit(link).toString());
+	}
 }
